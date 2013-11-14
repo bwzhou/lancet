5d31f5ef6bcfaeffc055a81f002af5e719efc025
Bowen Zhou <bwzhou@gmail.com>
2013-11-14 19:23:26 +0000

    added a tool to fill a template with a list of constants; updated run.kleaver to process entire directory.

---
+++ fill_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys, re
+from collections import deque
+
+constants = deque()
+
+with open(sys.argv[2]) as f:
+  for line in f:
+    for number in line.strip().split():
+      constants.append(number)
+
+output = []
+with open(sys.argv[1]) as f:
+  for line in f:
+    for tok in re.split(r'(\s+)', line):
+      if re.match('^\d+$', tok):
+        output.append(constants.popleft())
+      elif re.match('^\(\d+$', tok):
+        output.append('(' + constants.popleft())
+      elif re.match('^\d+\)$', tok):
+        output.append(constants.popleft() + ')')
+      else:
+        output.append(tok)
+print ''.join(output)
